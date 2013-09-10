0514bcaa1f05dc9daa01e406d9ab52b8beed5812
Teake Nutma <teake.nutma@gmail.com>
2013-09-10 14:23:57 +0200

    Added build file.

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,11 +1,17 @@
 (* Paclet Info File *)
 
-(* created 2013.09.09*)
+(* created 2013/09/10*)
 
 Paclet[
-  Name -> "MathLie",
-  Version -> "0.0.1",
-  MathematicaVersion -> "6+",
-  Extensions -> {
-    {"Documentation", Language -> "English"}
-}]
+    Name -> "MathLie",
+    Version -> "0.1.0",
+    MathematicaVersion -> "9+",
+    Description -> "A Mathematica interface to LiE",
+    Creator -> "Teake Nutma",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}
+        }
+]
+
+

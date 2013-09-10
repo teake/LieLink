c3b9cd022dc06f0275cc29bd58c6cee35b5a8e49
Teake Nutma <teake.nutma@gmail.com>
2013-09-10 19:38:10 +0200

    Added Mathematica version check.

--- MathLie/Kernel/init.m
+++ MathLie/Kernel/init.m
@@ -1,3 +1,11 @@
 (* Mathematica Init File *)
 
+General::mmaversion = "MathLie is incompatible with Mathematica `1`.";
+
+(* Check for MMA version. *)
+If[System`$VersionNumber < 7.,
+	Message[General::mmaversion, IntegerPart @ System`$VersionNumber];
+	Abort[]
+];
+
 Get[ "MathLie`MathLie`"]

9d7bab8f0c702acccc7ed2b231502f8bb181cffe
Teake Nutma <teake.nutma@gmail.com>
2013-09-12 09:53:26 +0200

    Added version number and printed info on package load.

--- LieLink/Kernel/init.m
+++ LieLink/Kernel/init.m
@@ -8,4 +8,15 @@ If[System`$VersionNumber < 7.,
 	Abort[]
 ];
 
+(* Get the version number. *)
+LieLink`$LieLinkVersion = Get["LieLink`VersionNumber`"];
+
+(* Print version info etc. *)
+Print["=================================================================="];
+Print["Package LieLink` version ", LieLink`$LieLinkVersion[[1]],", " ,LieLink`$LieLinkVersion[[2]]];
+Print["CopyRight (C) 2013, Teake Nutma, under the General Public License."];
+Print["Based on LiE, by Arjen M. Cohen, Marc van Leeuwen, and Bert Lisser."];
+Print["=================================================================="];
+
+(* Load the package. *)
 Get[ "LieLink`LieLink`"]

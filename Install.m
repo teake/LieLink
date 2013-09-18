7ff61462f0840bbbe50ede8c1a31b6fab3b01d32
Teake Nutma <teake.nutma@gmail.com>
2013-09-18 17:17:06 +0200

    Added automated install file.

---
+++ Install.m
@@ -0,0 +1,11 @@
+(* LieLink automatic installer.*)
+
+(* Load Rolf Mertig's package installer. *)
+Import["https://packageinstaller.googlecode.com/hg/PackageInstaller/PackageInstaller.m"];
+
+(* Install LieLink. *)
+InstallPackage[
+	"https://github.com/teake/LieLink/releases/download/0.4.0/LieLink.0.4.0.zip", 
+	Directory -> FileNameJoin[{$UserBaseDirectory, "Applications"}], 
+	Print -> False
+];
